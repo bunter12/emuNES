7dee3c125b6cc85316b6e851c99d85a43bbc6adf
bunter12 <home52690@gmail.com>
2025-11-06 18:05:34 +0300

    upd bus

--- include/bus.h
+++ include/bus.h
@@ -1,20 +1,17 @@
 #ifndef BUS_H
 #define BUS_H
-#include "cpu.h:
-class Bus {
-public:
+#include <cpu.h>
+#include <ppu.h>
 
+class Bus{
+public:
+    void cpu_write(uint16_t address, uint8_t data);
+    uint8_t cpu_read(uint16_t address);
+    
+private:
     CPU cpu;
     PPU ppu;
-    APU apu;
-    Controller controller1, controller2;
-    Mapper* mapper;
-
-    uint8_t ram[2048];
-
-    uint8_t cpuRead(uint16_t addr);
-    void cpuWrite(uint16_t addr, uint8_t data);
-
+    std::array<uint8_t, 2048> cpu_ram;
 };
 
 #endif //BUS_H
---
+++ src/bus.cpp
@@ -0,0 +1,19 @@
+#include <bus.h>
+
+void Bus::cpu_write(uint16_t address, uint8_t data) {
+    if (address >= 0x0000 && address <= 0x1FFF) {
+        cpu_ram[address & 0x07FF] = data;
+    } else if (address >= 0x2000 && address <= 0x3FFF) {
+//        ppu.cpu_write(address & 0x0007, data);
+    }
+}
+
+uint8_t Bus::cpu_read(uint16_t address) {
+    uint8_t data = 0x00;
+    if (address >= 0x0000 && address <= 0x1FFF) {
+        data = cpu_ram[address & 0x07FF];
+    } else if (address >= 0x2000 && address <= 0x3FFF) {
+//        data = ppu.cpu_read(address & 0x0007);
+    }
+    return data;
+}
